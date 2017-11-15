abc042c04a569fb7490e4e06d6005e9c32821605
programeri <programeri@ucione.local>
2017-11-15 18:13:53 +0100

    Init

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request; /* u ovom servisu se nalaze sve sve sve infomracije o requestu */
+
+class HomeController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return view('home');
+    }
+
+	public function aboutUs()
+	{
+		return view('about_us');
+	}
+}
